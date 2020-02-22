1a1672b8bb17c6c4c2894861f4f8c4c2ff16bfad
Ojas Sinha <sinhaojas67@gmail.com>
2020-02-22 03:49:01 +0000

    Revert "Include our qcom pathmap stuff"
    This reverts commit 0a1058e4d3d0632058ebf5eca152647f2d2d783a.

--- core/config.mk
+++ core/config.mk
@@ -238,7 +238,6 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(CYGNUS_BUILD),)
 include vendor/cygnus/configs/BoardConfigCygnus.mk
-include vendor/cygnus/build/core/qcom_target.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
